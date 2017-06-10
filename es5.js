4842bc9970d69e2983bcf0c68983c66e938e1cb7
Edward Bramanti <edward@bramanti.org>
2017-06-09 17:27:24 -0700

    Wire up new configuration

--- es5.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "extends": [
-        "eslint-config-airbnb-base/legacy",
-        "./base",
-    ].map(require.resolve),
-    "rules": {
-        "strict": [2, "function"],
-    },
-};
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 module.exports = {
     "extends": [
-        "eslint-config-airbnb-base",
-        "./base",
+        "./rules/possible-errors",
+        "./rules/best-practices",
+        "./rules/strict",
+        "./rules/variables",
+        "./rules/stylistic-issues",
+        "./rules/es6",
+        "./rules/plugins/import",
     ].map(require.resolve),
-    "rules": {
-        "new-cap": ["error", {
-            newIsCap: true,
-            capIsNewExceptions: ["express.Router"],
-        }],
-        "camelcase": ["off", { "properties": "always" }],
-    },
 };
