cdf2ef0eb51ec9bce3283d9b2ddb1f614a06f769
Edward Bramanti <edward@bramanti.org>
2017-06-12 10:45:46 -0700

    Add unicorn rules

---
+++ rules/plugins/unicorn.js
@@ -0,0 +1,21 @@
+module.exports = {
+    env: {
+        es6: true,
+    },
+    rules: {
+        "unicorn/catch-error-name": "off",
+        "unicorn/explicit-length-check": "error",
+        "unicorn/filename-case": "off",
+        "unicorn/no-abusive-eslint-disable": "error",
+        "unicorn/no-process-exit": "off",
+        "unicorn/throw-new-error": "error",
+        "unicorn/number-literal-case": "error",
+        "unicorn/escape-case": "error",
+        "unicorn/no-array-instanceof": "error",
+        "unicorn/no-new-buffer": "off",
+        "unicorn/no-hex-escape": "error",
+        "unicorn/custom-error-definition": "error",
+        "unicorn/prefer-starts-ends-with": "error",
+        "unicorn/prefer-type-error": "error",
+    },
+};
