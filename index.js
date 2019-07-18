067865dbcc3a464fa276e565107c3ad7bac7f54d
bvamp <bmartin@videoamp.com>
2019-07-18 15:36:01 -0700

    clean out the attic

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 module.exports = {
-    "extends": [
-        "./rules/possible-errors",
-        "./rules/best-practices",
-        "./rules/strict",
-        "./rules/variables",
-        "./rules/stylistic-issues",
-        "./rules/es6",
-        "./rules/plugins/import",
-        "./rules/plugins/promise",
-        "./rules/plugins/unicorn",
-        "./rules/plugins/fp",
-        "./rules/plugins/chai-expect",
-        "./rules/plugins/chai-friendly",
-    ].map(require.resolve),
+    "env": {
+        "node": true,
+        "browser": true,
+        "es6": true
+    }
 };
--- rules/plugins/chai-expect.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "plugins": ["chai-expect"],
-    "rules": {
-        "chai-expect/no-inner-compare": "error",
-        "chai-expect/missing-assertion": "error",
-        "chai-expect/terminating-properties": "error",
-    },
-};
