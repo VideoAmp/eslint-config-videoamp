3c474ec2e7386ef16be6bfe0e69fd3e8ac29633e
Zach Bolan <zbolan@ip-192-168-1-29.ec2.internal>
2022-05-07 09:55:15 -0700

    feat(measurement-api): [ENG-34849] Update eslint rules

--- base.js
+++ base.js
@@ -6,7 +6,7 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     ],
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     parserOptions: {
@@ -18,7 +18,7 @@ module.exports = {
         '@typescript-eslint/camelcase': 'off',
         // Public is assumed by default for class members, and shouldn't be explicit
         '@typescript-eslint/explicit-member-accessibility': 'off',
-        // It's often not feasible to avoid an "any" type for some siuations
+        // It's often not feasible to avoid an "any" type for some situations
         '@typescript-eslint/no-explicit-any': 'warn',
         // We often can't control API JSON response/request property naming conventions
         '@typescript-eslint/prefer-interface': 'off',
